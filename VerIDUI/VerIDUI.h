8cb81c9a4f0c8884c02a4000532e880ce6a69f6c
Jakub Dolejs <jakubdolejs@gmail.com>
2019-02-04 16:15:07 +0000

    Initial Commit

---
+++ VerIDUI/VerIDUI.h
@@ -0,0 +1,19 @@
+//
+//  VerIDUI.h
+//  VerIDUI
+//
+//  Created by Jakub Dolejs on 04/02/2019.
+//  Copyright © 2019 Applied Recognition. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VerIDUI.
+FOUNDATION_EXPORT double VerIDUIVersionNumber;
+
+//! Project version string for VerIDUI.
+FOUNDATION_EXPORT const unsigned char VerIDUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VerIDUI/PublicHeader.h>
+
+
