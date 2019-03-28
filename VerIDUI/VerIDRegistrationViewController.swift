3eaeae104f74542121a4bd278cae6461df99e61a
Jakub Dolejs <jakubdolejs@gmail.com>
2019-03-28 14:43:17 +0000

    Fixed drawing of registered faces in registration view

--- VerIDUI/VerIDRegistrationViewController.swift
+++ VerIDUI/VerIDRegistrationViewController.swift
@@ -14,8 +14,6 @@ class VerIDRegistrationViewController: VerIDViewController {
 
     @IBOutlet var detectedFaceStackView: UIStackView!
     
-    var requestedBearing: Bearing?
-    
     public init() {
         super.init(nibName: "VerIDRegistrationViewController")
     }
@@ -24,25 +22,82 @@ class VerIDRegistrationViewController: VerIDViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        guard let delegate = self.delegate else {
+    override func drawFaceFromResult(_ faceDetectionResult: FaceDetectionResult, sessionResult: SessionResult, defaultFaceBounds: CGRect, offsetAngleFromBearing: EulerAngle?) {
+        super.drawFaceFromResult(faceDetectionResult, sessionResult: sessionResult, defaultFaceBounds: defaultFaceBounds, offsetAngleFromBearing: offsetAngleFromBearing)
+        guard self.detectedFaceStackView.arrangedSubviews.isEmpty || (sessionResult.error == nil && faceDetectionResult.status == .faceAligned) else {
             return
         }
-        for _ in 0..<delegate.settings.numberOfResultsToCollect {
-            let imageView = createImageView()
-            detectedFaceStackView.addArrangedSubview(imageView)
+        guard let settings = self.delegate?.settings as? RegistrationSessionSettings else {
+            return
+        }
+        if self.detectedFaceStackView.arrangedSubviews.isEmpty {
+            for _ in 0..<settings.numberOfResultsToCollect {
+                let imageView = createImageView()
+                detectedFaceStackView.addArrangedSubview(imageView)
+            }
+        }
+        for i in 0..<settings.numberOfResultsToCollect {
+            guard let imageView = detectedFaceStackView.arrangedSubviews[i] as? UIImageView else {
+                return
+            }
+            for sub in imageView.subviews {
+                sub.removeFromSuperview()
+            }
+            let viewSize = imageView.frame.size
+            let bearingIndex = i % settings.bearingsToRegister.count
+            let bearing = settings.bearingsToRegister[bearingIndex]
+            var image: UIImage? = self.imageForBearing(bearing)
+            imageView.alpha = 0.5
+            imageView.transform = CGAffineTransform.identity
+            if i < sessionResult.attachments.count {
+                let attachment = sessionResult.attachments[i]
+                if attachment.bearing == bearing, let path = attachment.imageURL?.path, let faceImage = UIImage(contentsOfFile: path) {
+                    let originalBounds = attachment.face.bounds
+                    var scaledBoundsSize = CGSize(width: originalBounds.width, height: originalBounds.height)
+                    if viewSize.width / viewSize.height > originalBounds.width / originalBounds.height {
+                        // View is "fatter" match widths
+                        scaledBoundsSize.width = viewSize.width
+                        scaledBoundsSize.height = viewSize.width / originalBounds.width * originalBounds.height
+                    } else {
+                        scaledBoundsSize.height = viewSize.height
+                        scaledBoundsSize.width = viewSize.height / originalBounds.height * originalBounds.width
+                    }
+                    let transform = CGAffineTransform(scaleX: scaledBoundsSize.width / originalBounds.width, y: scaledBoundsSize.height / originalBounds.height)
+                    let bounds = attachment.face.bounds.applying(transform)
+                    let imageTransform = CGAffineTransform(scaleX: scaledBoundsSize.width / originalBounds.width, y: scaledBoundsSize.height / originalBounds.height)
+                    let scaledImageSize = faceImage.size.applying(imageTransform)
+                    UIGraphicsBeginImageContext(bounds.size)
+                    faceImage.draw(in: CGRect(x: 0-bounds.minX, y: 0-bounds.minY, width: scaledImageSize.width, height: scaledImageSize.height))
+                    image = UIGraphicsGetImageFromCurrentImageContext()
+                    UIGraphicsEndImageContext()
+                    imageView.alpha = 1
+                    if settings.cameraPosition == .front {
+                        imageView.transform = CGAffineTransform(scaleX: -1, y: 1)
+                    }
+                }
+            }
+            imageView.image = image
+            if i+1 == sessionResult.attachments.count {
+                let activityIndicatorView = UIActivityIndicatorView(frame: detectedFaceStackView.arrangedSubviews[i].bounds)
+                activityIndicatorView.startAnimating()
+                imageView.addSubview(activityIndicatorView)
+            }
         }
     }
     
-    override func drawFaceFromResult(_ faceDetectionResult: FaceDetectionResult, sessionResult: SessionResult, defaultFaceBounds: CGRect, offsetAngleFromBearing: EulerAngle?) {
-        super.drawFaceFromResult(faceDetectionResult, sessionResult: sessionResult, defaultFaceBounds: defaultFaceBounds, offsetAngleFromBearing: offsetAngleFromBearing)
-        guard sessionResult.error == nil && (requestedBearing == nil || requestedBearing! != faceDetectionResult.requestedBearing || faceDetectionResult.status == .faceAligned) else {
-            return
-        }
-        requestedBearing = faceDetectionResult.requestedBearing
+    func createImageView() -> UIImageView {
+        let imageView = UIImageView(image: nil)
+        let constraint = NSLayoutConstraint(item: imageView, attribute: .width, relatedBy: .equal, toItem: imageView, attribute: .height, multiplier: 4/5, constant: 0)
+        imageView.addConstraint(constraint)
+        imageView.contentMode = .scaleAspectFill
+        imageView.layer.cornerRadius = 12
+        imageView.layer.masksToBounds = true
+        return imageView
+    }
+
+    private func imageForBearing(_ bearing: Bearing) -> UIImage? {
         let imageName: String
-        switch faceDetectionResult.requestedBearing {
+        switch bearing {
         case .straight:
             imageName = "head_thumbnail_straight"
         case .up:
@@ -63,74 +118,6 @@ class VerIDRegistrationViewController: VerIDViewController {
             imageName = "head_thumbnail_left_up"
         }
         let bundle = Bundle(for: type(of: self))
-        guard let image = UIImage(named: imageName, in: bundle, compatibleWith: nil) else {
-            return
-        }
-        guard let settings = self.delegate?.settings else {
-            return
-        }
-        for i in 0..<settings.numberOfResultsToCollect {
-            guard let imageView = detectedFaceStackView.arrangedSubviews[i] as? UIImageView else {
-                return
-            }
-            for sub in imageView.subviews {
-                sub.removeFromSuperview()
-            }
-            let viewSize = imageView.frame.size
-            let images: [UIImage] = sessionResult.attachments.compactMap({
-                guard $0.bearing == self.requestedBearing else {
-                    return nil
-                }
-                guard let path = $0.imageURL?.path, let image = UIImage(contentsOfFile: path) else {
-                    return nil
-                }
-                let originalBounds = $0.face.bounds
-                var scaledBoundsSize = CGSize(width: originalBounds.width, height: originalBounds.height)
-                if viewSize.width / viewSize.height > originalBounds.width / originalBounds.height {
-                    // View is "fatter" match widths
-                    scaledBoundsSize.width = viewSize.width
-                    scaledBoundsSize.height = viewSize.width / originalBounds.width * originalBounds.height
-                } else {
-                    scaledBoundsSize.height = viewSize.height
-                    scaledBoundsSize.width = viewSize.height / originalBounds.height * originalBounds.width
-                }
-                let transform = CGAffineTransform(scaleX: scaledBoundsSize.width / originalBounds.width, y: scaledBoundsSize.height / originalBounds.height)
-                let bounds = $0.face.bounds.applying(transform)
-                let imageTransform = CGAffineTransform(scaleX: scaledBoundsSize.width / originalBounds.width, y: scaledBoundsSize.height / originalBounds.height)
-                let scaledImageSize = image.size.applying(imageTransform)
-                UIGraphicsBeginImageContext(bounds.size)
-                defer {
-                    UIGraphicsEndImageContext()
-                }
-                image.draw(in: CGRect(x: 0-bounds.minX, y: 0-bounds.minY, width: scaledImageSize.width, height: scaledImageSize.height))
-//                image.draw(at: CGPoint(x: 0-bounds.minX, y: 0-bounds.minY))
-                return UIGraphicsGetImageFromCurrentImageContext()
-            })
-            if i<images.count {
-                imageView.alpha = 1
-                imageView.image = images[i]
-                imageView.transform = CGAffineTransform(scaleX: -1, y: 1)
-            } else {
-                imageView.alpha = 0.5
-                imageView.image = image
-                imageView.transform = CGAffineTransform.identity
-            }
-            if i+1 == images.count {
-                let activityIndicatorView = UIActivityIndicatorView(frame: detectedFaceStackView.arrangedSubviews[i].bounds)
-                activityIndicatorView.startAnimating()
-                imageView.addSubview(activityIndicatorView)
-            }
-        }
+        return UIImage(named: imageName, in: bundle, compatibleWith: nil)
     }
-    
-    func createImageView() -> UIImageView {
-        let imageView = UIImageView(image: nil)
-        let constraint = NSLayoutConstraint(item: imageView, attribute: .width, relatedBy: .equal, toItem: imageView, attribute: .height, multiplier: 4/5, constant: 0)
-        imageView.addConstraint(constraint)
-        imageView.contentMode = .scaleAspectFill
-        imageView.layer.cornerRadius = 12
-        imageView.layer.masksToBounds = true
-        return imageView
-    }
-
 }
