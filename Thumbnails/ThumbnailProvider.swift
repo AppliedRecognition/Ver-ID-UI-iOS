4476f35677a56f9790aa81325935a88d9d9a9ec0
Jakub Dolejs <jakubdolejs@gmail.com>
2022-06-30 18:25:07 +0100

    Thumbnail and preview extensions updates

--- Thumbnails/ThumbnailProvider.swift
+++ Thumbnails/ThumbnailProvider.swift
@@ -20,43 +20,24 @@ class ThumbnailProvider: QLThumbnailProvider {
         // There are three ways to provide a thumbnail through a QLThumbnailReply. Only one of them should be used.
         
         // First way: Draw the thumbnail into the current context, set up with UIKit's coordinate system.
-        handler(QLThumbnailReply(contextSize: request.maximumSize, currentContextDrawing: { () -> Bool in
+        
+        if request.fileURL.pathExtension == "registration" {
             do {
-                let image: UIImage
-                if request.fileURL.pathExtension == "zip" {
-                    guard let archive = Archive(url: request.fileURL, accessMode: .read) else {
-                        return false
-                    }
-                    guard let imageEntry = archive.first(where: { entry in
-                        entry.path.contains("image")
-                    }) else {
-                        return false
-                    }
-                    var imageData = Data()
-                    _ = try archive.extract(imageEntry) { data in
-                        imageData.append(data)
-                    }
-                    guard let img = UIImage(data: imageData) else {
-                        return false
-                    }
-                    image = img
-                    // Return true if the thumbnail was successfully drawn inside this block.
+                let registration = try RegistrationImport.registration(from: request.fileURL)
+                let reply = QLThumbnailReply(contextSize: request.maximumSize) {
+                    let rect = AVMakeRect(aspectRatio: request.maximumSize, insideRect: CGRect(origin: .zero, size: registration.image.size))
+                    let scale = request.maximumSize.width / rect.width
+                    let cropRect = CGRect(origin: CGPoint(x: 0-rect.minX*scale, y: 0-rect.minY*scale), size: registration.image.size.applying(CGAffineTransform(scaleX: scale, y: scale)))
+                    registration.image.draw(in: cropRect)
                     return true
-                } else if request.fileURL.pathExtension == "verid" {
-                    let registrationData = try RegistrationImport.registration(from: request.fileURL)
-                    image = registrationData.image
-                } else {
-                    return false
                 }
-                let rect = AVMakeRect(aspectRatio: request.maximumSize, insideRect: CGRect(origin: .zero, size: image.size))
-                let scale = request.maximumSize.width / rect.width
-                let cropRect = CGRect(origin: CGPoint(x: 0-rect.minX*scale, y: 0-rect.minY*scale), size: image.size.applying(CGAffineTransform(scaleX: scale, y: scale)))
-                image.draw(in: cropRect)
-                return true
+                DispatchQueue.main.async {
+                    handler(reply, nil)
+                }
             } catch {
-                return false
+                handler(nil, error)
             }
-        }), nil)
+        }
         
         /*
         
